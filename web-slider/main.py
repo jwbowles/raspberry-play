37b3ef108f240c07193cfecc82b8b2e92a49d0c7
jwbowles <jimmywb+git@gmail.com>
2013-10-03 16:13:38 -0700

    Multiple Sliders
    Updated web-slider code to unclude 3 sliders.

--- web-slider/main.py
+++ web-slider/main.py
@@ -3,16 +3,26 @@ from quick2web import WebServer, SharedValue
 def main():
   webserver = WebServer(port=8888, debug=True)
 
-  webserver.websocket('/slider-value',
-      SharedValue(20, on_change=slider_updated))
+  webserver.websocket('/volume-value',
+      SharedValue(20, on_change=volume_updated))
+  webserver.websocket('/tone-value',
+      SharedValue(20, on_change=tone_updated))
+  webserver.websocket('/sustain-value',
+      SharedValue(20, on_change=sustain_updated))
 
   webserver.static_files('/', './static')
   print('Listening on %s' % webserver.url)
   webserver.run()
 
-def slider_updated(value, connection):
+def volume_updated(value, connection):
   # TODO: Communicate with LED bar graph
-  print('Slider value: ', value)
+  print('Volume value: ', value)
+
+def tone_updated(value, connection):
+  print('Tone value: ', value)
+
+def sustain_updated(value, connection):
+  print('Sustain value: ', value)
 
 if __name__ == '__main__':
   main()
